1524a37c4c9b6c47c4dc4ac6cd9122ab70470bd6
kamesan <kamesan.tokotoko@asagi.waseda.jp>
2020-06-28 17:05:11 +0900

    fourthdcommit

---
+++ practice.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Write something funny.\n");
+    char str[100];
+    scanf("%s",str);
+    printf("Exactlly, '%s' is funny. \n",str);
+    return 0;
+}
